04dc7ced0d116e85f70f9bcb2ae1f31b171fb1f3
Marek Chalupa <mchqwerty@gmail.com>
2017-09-07 14:50:25 +0200

    add database scheme
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

---
+++ database_scheme.sql
@@ -0,0 +1,41 @@
+CREATE TABLE `tool` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `version` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `benchmarks_set` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `tool_run` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tool_id` int(11) DEFAULT NULL,
+  `options` text,
+  `memlimit` varchar(100) DEFAULT NULL,
+  `cpulimit` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`tool_id`) REFERENCES `tool` (`id`)
+);
+
+CREATE TABLE `run` (
+  `status` varchar(255) DEFAULT NULL,
+  `cputime` float DEFAULT NULL,
+  `walltime` float DEFAULT NULL,
+  `memusage` int(11) DEFAULT NULL,
+  `classification` varchar(50) DEFAULT NULL,
+  `exitcode` int(11) DEFAULT NULL,
+  `exitsignal` int(11) DEFAULT NULL,
+  `terminationreason` varchar(100) DEFAULT NULL,
+  `tool_run_id` int(11) NOT NULL,
+  `benchmarks_set_id` int(11) NOT NULL,
+  `property` varchar(100) DEFAULT NULL,
+  `options` text COLLATE,
+  `file` text COLLATE,
+  FOREIGN KEY (`tool_run_id`) REFERENCES `tool_run` (`id`),
+  FOREIGN KEY (`benchmarks_set_id`) REFERENCES `benchmarks_set` (`id`)
+);
+
