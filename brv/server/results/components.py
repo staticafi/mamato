3db1799128ae151bad891fee3562f1a8e8d2c885
Tomas Jasek <tomsik68@gmail.com>
2018-03-27 16:36:27 +0200

    results: start working on component system
    Signed-off-by: Tomas Jasek <tomsik68@gmail.com>

---
+++ brv/server/results/components.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+
+from math import ceil
+
+# define utility functions here
+def formatTime(time):
+    "Transform time in seconds to hours, minutes and seconds"
+    if not time:
+        return '0 s'
+    ret = ''
+    time = ceil(time)
+    if time >= 3600:
+        hrs = time // 3600
+        time = time - hrs*3600
+        ret = '{0} h'.format(int(hrs))
+    if time >= 60 or ret != '':
+        mins = time // 60
+        time = time - mins*60
+        ret += ' {0} min'.format(int(mins))
+    if ret != 0:
+        return ret + ' {0} s'.format(int(time))
+    else:
+        return ret + '{0} s'.format(int(time))
+
+class CategoryComponent:
+    def __init__(self):
+        pass
+
+    def getDisplayName(self):
+        pass
+
+    def render(self, run, stats):
+        return self.getValue(run, stats)
+
+    def getValue(self, run, stats):
+        pass
+
+class CategoryTimeComponent(CategoryComponent):
+    def __init__(self):
+        pass
+
+    def getDisplayName(self):
+        return 'CPU Time:'
+
+    """
+    Return String
+    """
+    def getValue(self, run, stats):
+        result = '0'
+        if stats:
+            result = formatTime(stats.getAccTime())
+        return result
+
+# define CategoryComponent-s here
+
+class BucketComponent:
+    def __init__(self):
+        pass
+
+    def getDescription(self):
+        pass
+
+    """
+    Return string
+    """
+    def render(self, run, bucket, stats):
+        return self.getValue(run, bucket, stats)
+
+    """
+    Return String
+    """
+    def getValue(self, run, bucket, stats):
+        pass
+
+class BucketCountComponent(BucketComponent):
+    def __init__(self):
+        pass
+
+    def getDescription(self):
+        return 'count of results'
+
+    def render(self, run, bucket, stats):
+        return ''.join([
+                '<span class="', bucket.getNameClass(), '">',
+                '<abbr title="', bucket.getDisplayName(), '">',
+                self.getValue(run, bucket, stats),
+                '</abbr></span>'
+        ])
+
+    def getValue(self, run, bucket, stats):
+        result = 0
+        if stats:
+            for classif in bucket.getClassifications():
+                result += stats.getCount(classif)
+        return str(result)
+
+class BucketTimeComponent(BucketComponent):
+    def __init__(self):
+        pass
+
+    def getDescription(self):
+        return 'Time elapsed'
+
+    def render(self, run, bucket, stats):
+        return ''.join([
+            '<span style="font-size: 11px">',
+            self.getValue(run, bucket, stats),
+            '</span>'
+        ])
+    def getValue(self, run, bucket, stats):
+        result = 0
+        if stats:
+            for classif in bucket.getClassifications():
+                result += stats.getCount(classif)
+        return formatTime(result)
+
+# define BucketComponent-s here
--- brv/server/showresults.py
+++ brv/server/showresults.py
@@ -4,180 +4,156 @@ from . rendering import render_template
 from . util import get_elem, getDescriptionOrVersion
 from math import ceil
 from .. import groupingmanager
+from . results.components import BucketTimeComponent, CategoryTimeComponent, BucketCountComponent
+
+class ResultsView:
+    def __init__(self, runs, buckets, bucket_components, categories, category_components, groupings, opts):
+        self._runs = runs
+        self._buckets = buckets
+        self._bucket_components = bucket_components
+        self._categories = categories
+        self._category_components = category_components
+        self._opts = opts
+        self._groupings = groupings
+
+    # TODO this method is way too long
+    @classmethod
+    def crunchData(cls, datamanager, runs, times_only_solved, grouping):
+        def hasAnswers(runs, bset_id, classif):
+            assert not runs is None
+            assert not bset_id is None
+
+            for run in runs:
+                if bset_id != -1:
+                    # get stats for one of the categories
+                    stats = run.getStats().getStatsByID(bset_id)
+                else:
+                    # get the overall stats
+                    stats = run.getStats().getSummary(times_only_solved)
+                if not stats is None and stats.getCount(classif) != 0:
+                    return True
+
+            return False
+
+        def bucketHasAnswers(runs, bucket):
+            classifs = bucket.getClassifications()
+            for classif in classifs:
+                if hasAnswers(runs, -1, classif):
+                    return True
+            return False
+
+        class BSet(object):
+            def __init__(self, name, bid):
+                self.name = name
+                self.id = bid
+
+            def __hash__(self):
+                return self.id
+
+            def __eq__(self, oth):
+                return self.id == oth.id
+
+        categories = set()
+        buckets = grouping.getBuckets()
+        # there's few of classifications usually, it will be faster in list
+        classifications = []
+        for run in runs:
+            run._stats = datamanager.getToolInfoStats(run.getID())
+            for stats in run._stats.getAllStats().values():
+                stats.accumulateTime(times_only_solved)
+                # a pair (name, id)
+                categories.add(BSet(stats.getBenchmarksName(), stats.getBenchmarksID()))
+                for c in stats.getClassifications():
+                    if c not in classifications:
+                        classifications.append(c)
+
+        # extend the selected grouping to avoid hiding non-configured results
+        for c in classifications:
+            found = False
+            for b in buckets:
+                if c in b.getClassifications():
+                    found = True
+            if not found:
+                display_name = c[0]
+                if display_name == None or len(display_name) == 0:
+                    display_name = "<i>&lt;missing classification&gt;</i>"
+                buckets.append(groupingmanager.GroupingBucket(display_name, "classif status-{0}".format(c[1]), [c]))
+
+        # only show buckets with results
+        buckets = list(filter(lambda b: bucketHasAnswers(runs, b), buckets))
+        buckets = list(zip(buckets, range(len(buckets))))
+        # give it some fixed order
+        cats = [x for x in categories]
+        return (buckets, cats)
+
+    @classmethod
+    def assemble(cls, datamanager, opts):
+        bucket_components = [BucketCountComponent()]
+        category_components = []
+
+        run_ids = list(map(int, opts['run']))
+        runs = datamanager.getToolRuns(run_ids)
+        # parse opts and add components accordingly
+        times_only_solved = 'show_times_only_solved' in opts
+        if 'show_times' in opts:
+            bucket_components.append(BucketTimeComponent())
+            category_components.append(CategoryTimeComponent())
+
+        # prepare grouping and categories
+        groupingId = 0
+        if 'grouping' in opts:
+            groupingId = int(opts['grouping'][0])
+        grouping = datamanager.getGrouping(groupingId)
+        if grouping is None:
+            grouping = datamanager.getGrouping(0)
+        (buckets, cats) = ResultsView.crunchData(datamanager, runs, times_only_solved, grouping)
+        groupings = datamanager.getGroupingChoices()
+        return cls(runs, buckets, bucket_components, cats, category_components, datamanager.getGroupingChoices(), opts)
+
+    def render(self, wfile):
+        def _toolsGETList():
+            s = ''
+            for x in self._opts['run']:
+                s += '&run={0}'.format(x)
+            return s
+
+        def _getStats(run, bset_id):
+            assert not run is None
+            assert not bset_id is None
+
+            return run.getStats().getStatsByID(bset_id)
+
+        def _getTotalStats(run):
+            assert not run is None
+            return run.getStats().getSummary(times_only_solved)
+
+        def _packStatsFunc(bset_id):
+            return (lambda run: _getStats(run, bset_id))
+
+        def _lenPlus(o, a):
+            return len(o) + a
+
+        times_only_solved = 'show_times_only_solved' in self._opts
+
+        render_template(wfile, 'results.html',
+                     {'runs':self._runs, 'benchmarks_sets' : self._categories,
+                      'toolsGETList' : _toolsGETList,
+                      'getStats' : _getStats,
+                      'getTotalStats' : _getTotalStats,
+                      'get' : get_elem,
+                      'descr' : getDescriptionOrVersion,
+                      'buckets': self._buckets,
+                      'packStatsFunc': _packStatsFunc,
+                      'inlineView': 'inline_view' in self._opts,
+                      'bucketComponents': self._bucket_components,
+                      'lenPlus': _lenPlus,
+                      'categoryComponents': self._category_components,
+                      'groupings': self._groupings })
 
 def showResults(wfile, datamanager, opts):
     if not 'run' in opts:
         wfile.write(b'<h2>No runs of tools given</h2>')
         return
 
-    def hasAnswers(runs, bset_id, classif):
-        assert not runs is None
-        assert not bset_id is None
-
-        for run in runs:
-            if bset_id != -1:
-                # get stats for one of the categories
-                stats = run.getStats().getStatsByID(bset_id)
-            else:
-                # get the overall stats
-                stats = run.getStats().getSummary(_showTimesOnlySolved)
-            if not stats is None and stats.getCount(classif) != 0:
-                return True
-
-        return False
-
-    def bucketHasAnswers(runs, bucket):
-        classifs = bucket.getClassifications()
-        for classif in classifs:
-            if hasAnswers(runs, -1, classif):
-                return True
-        return False
-
-    class BSet(object):
-        def __init__(self, name, bid):
-            self.name = name
-            self.id = bid
-
-        def __hash__(self):
-            return self.id
-
-        def __eq__(self, oth):
-            return self.id == oth.id
-
-    _showTimes = 'show_times' in opts
-    _showTimesOnlySolved = 'show_times_only_solved' in opts
-    groupingId = 0
-    if 'grouping' in opts:
-        groupingId = int(opts['grouping'][0])
-    grouping = datamanager.getGrouping(groupingId)
-    if grouping is None:
-        grouping = datamanager.getGrouping(0)
-    _inlineView = 'inline_view' in opts
-    buckets = grouping.getBuckets()
-    # list of ToolRun objects
-    run_ids = list(map(int, opts['run']))
-    runs = datamanager.getToolRuns(run_ids)
-    categories = set()
-    # there's few of classifications usually, it will be faster in list
-    classifications = []
-    for run in runs:
-        run._stats = datamanager.getToolInfoStats(run.getID())
-        for stats in run._stats.getAllStats().values():
-            stats.accumulateTime(_showTimesOnlySolved)
-            # a pair (name, id)
-            categories.add(BSet(stats.getBenchmarksName(), stats.getBenchmarksID()))
-            for c in stats.getClassifications():
-                if c not in classifications:
-                    classifications.append(c)
-
-    # extend the selected grouping to avoid hiding non-configured results
-    for c in classifications:
-        found = False
-        for b in buckets:
-            if c in b.getClassifications():
-                found = True
-        if not found:
-            display_name = c[0]
-            if display_name == None or len(display_name) == 0:
-                display_name = "<i>&lt;missing classification&gt;</i>"
-            buckets.append(groupingmanager.GroupingBucket(display_name, "classif status-{0}".format(c[1]), [c]))
-
-    buckets = list(filter(lambda b: bucketHasAnswers(runs, b), buckets))
-    buckets = list(zip(buckets, range(len(buckets))))
-    # give it some fixed order
-    cats = [x for x in categories]
-
-    def _toolsGETList():
-        s = ''
-        for x in opts['run']:
-            s += '&run={0}'.format(x)
-        return s
-
-    def _getStats(run, bset_id):
-        assert not run is None
-        assert not bset_id is None
-
-        return run.getStats().getStatsByID(bset_id)
-
-    def _getCount(stats, classif):
-        if stats:
-            return stats.getCount(classif)
-        else:
-            return 0
-
-    def _getBucketCount(stats, bucket):
-        if stats:
-            result = 0
-            for classif in bucket.getClassifications():
-                result += _getCount(stats, classif)
-            return result
-        else:
-            return 0
-
-
-    def _getTime(stats, classif):
-        if stats:
-            return ceil(stats.getTime(classif))
-        else:
-            return 0
-
-    def _getBucketTime(stats, bucket):
-        if stats:
-            result = 0
-            for classif in bucket.getClassifications():
-                result += _getTime(stats, classif)
-            return result
-        else:
-            return 0
-
-    def _getTotalStats(run):
-        assert not run is None
-
-        return run.getStats().getSummary(_showTimesOnlySolved)
-
-    def _lenPlus(o, a):
-        return len(o) + a
-
-    def _formatTime(time):
-        "Transform time in seconds to hours, minutes and seconds"
-        if not time:
-            return '0 s'
-        ret = ''
-        time = ceil(time)
-        if time >= 3600:
-            hrs = time // 3600
-            time = time - hrs*3600
-            ret = '{0} h'.format(int(hrs))
-        if time >= 60 or ret != '':
-            mins = time // 60
-            time = time - mins*60
-            ret += ' {0} min'.format(int(mins))
-        if ret != 0:
-            return ret + ' {0} s'.format(int(time))
-        else:
-            return ret + '{0} s'.format(int(time))
-
-    def _packStatsFunc(bset_id):
-        return (lambda run: _getStats(run, bset_id))
-
-    render_template(wfile, 'results.html',
-                     {'runs':runs, 'benchmarks_sets' : cats,
-                      'toolsGETList' : _toolsGETList,
-                      'getStats' : _getStats,
-                      'getTotalStats' : _getTotalStats,
-                      'getBucketCount' : _getBucketCount,
-                      'getCount' : _getCount,
-                      'getTime' : _getTime,
-                      'getBucketTime' : _getBucketTime,
-                      'get' : get_elem,
-                      'showTimes' : _showTimes,
-                      'showTimesOnlySolved' : _showTimesOnlySolved,
-                      'formatTime' : _formatTime,
-                      'descr' : getDescriptionOrVersion,
-                      'buckets': buckets,
-                      'groupingId': groupingId,
-                      'lenPlus': _lenPlus,
-                      'packStatsFunc': _packStatsFunc,
-                      'inlineView': _inlineView,
-                      'groupings': datamanager.getGroupingChoices() })
-
+    view = ResultsView.assemble(datamanager, opts)
+    view.render(wfile)
